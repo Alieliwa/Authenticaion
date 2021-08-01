d35742dbbe8df352f8da0829f249c81df2a3ddab
alieliwa <alieliwa515@gmail.com>
2021-08-01 07:23:20 +0200

    verification

--- app/src/main/java/com/example/authenticaion/LogInActivity.kt
+++ app/src/main/java/com/example/authenticaion/LogInActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_log_in.*
 import kotlinx.android.synthetic.main.activity_signin.*
 
@@ -19,22 +20,31 @@ class LogInActivity : AppCompatActivity() {
 
 
         btnLogin.setOnClickListener {
-            var mail = edit_Email.text.toString()
-            var password = editPassword.text.toString()
-            if (mail.isNotEmpty() && password.isNotEmpty()){
-                mAuth!!.signInWithEmailAndPassword(mail,password).addOnCompleteListener{
-                    if (it.isSuccessful){
-                        val intenttomain  = Intent(this,MainActivity::class.java)
-                        startActivity(intenttomain)
-                    }else {
-                        Toast.makeText(this,"invalid E_mail or Password", Toast.LENGTH_LONG).show()
-                    }
-                }
-            }else{
-                Toast.makeText(this,"E_mail or Password Is Empty", Toast.LENGTH_LONG).show()
-
-            }
-
+            login()
         }
     }
-}
+    private fun login(){
+        var mail = edit_Email.text.toString()
+        var password = editPassword.text.toString()
+        if (mail.isNotEmpty() && password.isNotEmpty()){
+            mAuth!!.signInWithEmailAndPassword(mail,password).addOnCompleteListener{
+                if (it.isSuccessful){
+                    verifyEmailAdd()
+                }else {
+                    Toast.makeText(this,"invalid E_mail or Password", Toast.LENGTH_LONG).show()
+                }
+            }
+        }else{
+            Toast.makeText(this,"E_mail or Password Is Empty", Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun verifyEmailAdd(){
+      var User  =   mAuth?.currentUser
+        if (User!!.isEmailVerified){
+            val intenttomain  = Intent(this,MainActivity::class.java)
+            startActivity(intenttomain)
+        }else{
+            Toast.makeText(this,"please verify your account....",Toast.LENGTH_LONG).show()
+        }
+    }
+    }
--- app/src/main/java/com/example/authenticaion/SignInActivity.kt
+++ app/src/main/java/com/example/authenticaion/SignInActivity.kt
@@ -1,5 +1,6 @@
 package com.example.authenticaion
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -16,23 +17,38 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
         mAuth =  FirebaseAuth.getInstance()
-        btnSignin.setOnClickListener {
-            var e_mail = edmail.text.toString()
-            var pass = edpass.text.toString()
-            if (e_mail.isNotEmpty()&&pass.isNotEmpty()){
-                progressBar.visibility = View.VISIBLE
-                progressBar.setBackgroundColor(Color.BLACK)
-                mAuth!!.createUserWithEmailAndPassword(e_mail,pass)
-                    .addOnCompleteListener {
-                        if (it.isSuccessful){
-                            Toast.makeText(applicationContext,"Is Successful",Toast.LENGTH_LONG).show()
-                            progressBar.visibility = View.GONE
-                        }else {
-                            Toast.makeText(applicationContext,it.exception.toString(),Toast.LENGTH_LONG).show()
-                        }
+        btnRejestter.setOnClickListener {
+            Rejestter()
+        }
+    }
+    private fun Rejestter(){
+        var e_mail = edmail.text.toString()
+        var pass = edpass.text.toString()
+        if (e_mail.isNotEmpty()&&pass.isNotEmpty()){
+            progressBar.visibility = View.VISIBLE
+            progressBar.setBackgroundColor(Color.BLACK)
+            mAuth!!.createUserWithEmailAndPassword(e_mail,pass)
+                .addOnCompleteListener {
+                    if (it.isSuccessful){
+                        sentverify()
+                        progressBar.visibility = View.GONE
+                    }else {
+                        Toast.makeText(applicationContext,it.exception.toString(),Toast.LENGTH_LONG).show()
                     }
+                }
+        }else{
+            Toast.makeText(applicationContext,"Empty",Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun sentverify(){
+     var user =    mAuth!!.currentUser
+        user!!.sendEmailVerification().addOnCompleteListener {
+
+            if (it.isSuccessful){
+                val intenttologin = Intent(this,LogInActivity::class.java)
+                startActivity(intenttologin)
             }else{
-                Toast.makeText(applicationContext,"Empty",Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext,it.exception.toString(),Toast.LENGTH_LONG).show()
             }
         }
     }
